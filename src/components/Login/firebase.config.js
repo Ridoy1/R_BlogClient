1efc324056d4c7323ceab3e2f25f600f94af2ac3
Ridoy1 <hridoykarmakar883@gmail.com>
2021-09-20 20:57:02 +0600

    added updata

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCOP74ipPdoD2WJbRC3ma813_D2z03TjGk",
+  authDomain: "blog-90d68.firebaseapp.com",
+  projectId: "blog-90d68",
+  storageBucket: "blog-90d68.appspot.com",
+  messagingSenderId: "397530514252",
+  appId: "1:397530514252:web:3e418e96108b2eca4f05e9"
+};
+
+export default firebaseConfig;
